eeb28842492f4e43365ff24fc40c707f8da35e76
Soichi Hayashi <hayashis@iu.edu>
2016-02-04 21:19:45 +0000

    yet another

--- api/services/index.js
+++ api/services/index.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('express-jwt');
-var _ = require('underscore');
+//var _ = require('underscore');
 
 //mine
 var config = require('../config');
