92611f4430f6a217b9c031cd6d45534b3435c5a4
Soichi Hayashi <soichih@gmail.com>
2016-07-28 15:42:07 +0000

    added debug out

--- test/install.sh
+++ test/install.sh
@@ -9,25 +9,11 @@ fi
 echo "installing test configuration"
 cp test/config/* config/
 
-#if [ ! -f config/auth.key ];
-#then
-#    echo "installing test auth.key /.pub"
-#    (
-#    cp test/config/auth.key config/auth.key
-#    cp test/config/auth.pub config/auth.pub
-#    )
-#fi
-#
-#if [ ! -f config/index.js ];
-#then
-#    echo "installing test config/index.js"
-#    cp test/config/index.js config/index.js 
-#fi
-#
-#if [ ! -f config/resources.js ];
-#then
-#    echo "installing test config/resources.js"
-#    cp test/config/resources.js config/resources.js 
-#fi
+echo "admin.jwt"
+cat config/admin.jwt 
 
+echo "auth.pub"
+cat config/auth.pub
 
+echo "auth.key"
+cat config/auth.key
