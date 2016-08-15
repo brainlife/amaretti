6435f321d7a622bd6daa563be0ee488c089c1f85
Soichi Hayashi <soichih@gmail.com>
2016-08-15 15:56:30 +0000

    placed correct start hook for package.json

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #DEBUG=sca:* env=dev PORT=12403 nodemon -i node_modules ./index.js
 
 pm2 delete sca-wf
-pm2 start api/sca.js --name sca-wf --watch --ignore-watch="*.log test *.sh ui bin example"
+pm2 start api/wf.js --name sca-wf --watch --ignore-watch="*.log test *.sh ui bin example"
 
 pm2 delete sca-wf-task
 pm2 start bin/sca-task.js --name sca-wf-task --watch --ignore-watch="*.log test *.sh ui example"
