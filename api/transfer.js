068cf97da76e16ec1cbbab2d1955fca30325f5e1
Soichi Hayashi <soichih@gmail.com>
2016-08-29 20:41:26 +0000

    removed -v which was showing progress status on stderr

--- api/transfer.js
+++ api/transfer.js
@@ -64,8 +64,9 @@ exports.rsync_resource = function(source_resource, dest_resource, source_path, d
                 //TODO need to investigate why I need these -o options on q6>karst transfer
                 var sshopts = "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i .sca/keys/"+source_resource._id+".sshkey";
                 var source = source_resource.config.username+"@"+hostname+":"+source_path+"/";
-                logger.debug("rsync -avL --progress -e \""+sshopts+"\" "+source+" "+dest_path);
-                conn.exec("rsync -avL --progress -e \""+sshopts+"\" "+source+" "+dest_path, function(err, stream) {
+                //-v writes output to stderr.. even though it's not error..
+                logger.debug("rsync -aL --progress -e \""+sshopts+"\" "+source+" "+dest_path);
+                conn.exec("rsync -aL --progress -e \""+sshopts+"\" "+source+" "+dest_path, function(err, stream) {
                     if(err) next(err);
                     stream.on('close', function(code, signal) {
                         if(code) return next("Failed to rsync content from source_resource:"+source_path+" to dest_resource:"+dest_path+" Please check firewall / sshd configuration / disk space");
