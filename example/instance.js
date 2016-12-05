b08d5c7c6289a63ec6c3a67f80dfa9ba95072fc0
Soichi Hayashi <soichih@gmail.com>
2016-12-05 16:20:28 +0000

    added an example script

---
+++ example/instance.js
@@ -0,0 +1,22 @@
+var request = require('request');
+var fs = require('fs');
+var sca = "https://soichi7.ppa.iu.edu/api";
+
+//update to point to your sca jwt (see login page for more info)
+var jwt = fs.readFileSync('/home/hayashis/.sca/keys/cli.jwt', {encoding: 'ascii'}).trim();
+
+request.post({
+    url: sca+"/wf/instance",
+    json: true,
+    headers: { 'Authorization': 'Bearer '+jwt },
+    body: {
+        workflow_id: 'ahoi',
+        name: 'ahoiahahaha',
+        desc: 'anything you want',
+    }, 
+}, function(err, res, body) {
+    if(err) throw err;
+    if(res.statusCode != 200) throw new Error(body);
+    console.dir(body);
+});
+
