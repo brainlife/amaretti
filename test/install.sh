06941c5be526a3364c374f44ef8638b0326a6a5a
stevengeeky <stevengeeky@gmail.com>
2017-08-10 08:50:16 -0400

    tests (4)

--- test/install.sh
+++ test/install.sh
@@ -2,6 +2,7 @@
 #invoked via .travis.yml
 
 ssh-agent -a ssh-agent.sock
+export SSH_AUTH_SOCK="$(pwd)/ssh-agent.sock"
 
 if [ -f config/index.js ]; then
     echo "config/index.js already exists.. skipping config installation"
