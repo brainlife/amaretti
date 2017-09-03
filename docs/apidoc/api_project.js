af5e0c9bc5ff17d19f096009359a0c547d034861
Soichi Hayashi <hayashis@iu.edu>
2017-09-03 22:48:39 +0000

    moved apidoc under /docs

--- docs/apidoc/api_project.js
+++ docs/apidoc/api_project.js
@@ -1,4 +1,4 @@
-{
+define({
   "name": "sca-wf",
   "version": "1.1.0",
   "description": "SCA Workflow Service",
@@ -7,8 +7,8 @@
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-08-16T12:19:48.363Z",
+    "time": "2017-09-03T22:48:04.620Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
-}
+});
