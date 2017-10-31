175f3ca3e44505c18ae20df50cbeb516460ae4f6
Soichi Hayashi <hayashis@iu.edu>
2017-10-31 12:53:11 +0000

    fixed incorrect uses of conn.sftp introduced by the use of connection queue. added resource usage counter to not count the task that is considered to break possible dead lock

--- bin/task.js
+++ bin/task.js
@@ -443,6 +443,7 @@ function handle_requested(task, next) {
         }
         _resource_select(user, task, function(err, resource, score, considered) {
             if(err) return next(err);
+            //logger.debug(considered);
             if(!resource) {
                 task.status_msg = "No resource currently available to run this task.. waiting.. ";
                 
