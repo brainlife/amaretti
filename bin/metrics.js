d43d14edf58320b6f8e8f4b40a8a59e4b66b7932
Soichi Hayashi <hayashis@iu.edu>
2018-08-02 19:23:32 +0000

    added task submitted and user.active metrics

--- bin/metrics.js
+++ bin/metrics.js
@@ -9,9 +9,10 @@ const winston = require('winston');
 const async = require('async');
 const redis = require('redis');
 
-//mine
 const config = require('../config');
+config.logger.winston.transports[0].level = 'error';
 const logger = new winston.Logger(config.logger.winston);
+const db = require('../api/models');
 
 const graphite_prefix = process.argv[2]||config.sensu.prefix;
 if(!graphite_prefix) {
@@ -173,6 +174,43 @@ request.get({
 
         re.end(true);
     });
+});
+
+function count_tasks(d) {
+    return new Promise((resolve, reject)=>{
+        db.Task.count({create_date: {$lt: d}}, (err, count)=>{
+            if(err) return reject(err);
+            const time = Math.round(d.getTime()/1000);
+            console.log(graphite_prefix+".task.count "+count+" "+time);
+            resolve();
+        });
+    });
+}
+
+function count_active_user(d) {
+    return new Promise((resolve, reject)=>{
+        db.Task.distinct('user_id', {create_date: {$lt: d}}, (err, users)=>{
+            if(err) return reject(err);
+            const time = Math.round(d.getTime()/1000);
+            console.log(graphite_prefix+".user.active "+users.length+" "+time);
+            resolve();
+        });
+    });
+}
+db.init(async function(err) {
+    if(err) throw err;
+    let today = new Date();
+    /*
+    let d = new Date("2017-01-01");
+    while(d.getTime() < today.getTime()) {
+        //await count_tasks(d); 
+        await count_active_user(d); 
+        d.setDate(d.getDate()+7);
+    }
+    */
+    await count_tasks(today); 
+    await count_active_user(today); 
+    db.disconnect();
 });
 
 
